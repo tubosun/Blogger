4044cf6b07841feacccb699feaab6247084dba4c
tubosun <snikyt@yahoo.com>
2016-09-23 09:49:40 -0400

    form-based feature added

--- Bloggers/app/controllers/articles_controller.rb
+++ Bloggers/app/controllers/articles_controller.rb
@@ -1,8 +1,34 @@
 class ArticlesController < ApplicationController
+			include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 	def show
 		@articles = Article.find(params[:id])
+	end
+	def new
+		@articles = Article.new
+	end
+	def create
+		@articles = Article.new(article_params)
+		@articles.save
+		flash.notice = "Article '#{@articles.title}' created!"
+		redirect_to article_path(@article) 
+	end
+	def destroy
+		@articles = Article.find(params[:id])
+		@articles.destroy
+		flash.notice = "Article '#{@articles.title}' deleted!"
+		redirect_to articles_path(@article)
+	end
+	def edit
+		@articles=Article.find(params[:id])
+	end
+	def update
+	@articles=Article.find(params[:id])
+	@articles.update(article_params)
+	
+	flash.notice = "Article '#{@articles.title}' updated!"
+	redirect_to articles_path(@article)
 	end
 end
